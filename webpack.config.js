64aa8f5561d81e9bd1f1e49eed591fbe3c72a18e
Mijung Lee <Mijung@Stephens-MacBook-Air.local>
2017-10-19 19:30:39 -0700

    Adding eslint rules, and enabling presets for babel.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,8 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
   devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    './src/index.jsx'
-  ],
+  entry: ['webpack-dev-server/client?http://localhost:3000', './src/index.jsx'],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
@@ -16,16 +12,17 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        loader: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'stage-0', 'react']
+          }
+        },
         include: path.join(__dirname, 'src')
       },
       {
         test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   }
