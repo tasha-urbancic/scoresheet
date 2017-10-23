26e5d3530b9dfbfde0dd107c3075040a75c7bb5b
Mijung Lee <Mijung@Stephens-MacBook-Air.local>
2017-10-23 12:40:06 -0700

    implemented TemplateList smart component to render inside of HomePage component

--- scoresheet-app/src/containers/TemplateList.jsx
+++ scoresheet-app/src/containers/TemplateList.jsx
@@ -1,66 +1,44 @@
-// logic for creating a list of all templates goes here
+import React, { Component } from 'react';
 
-// this could be a plain js file that queries for templates
-
-import React, { Component } from "react";
-
-import { connect } from "react-redux";
-
-// import HomePage from "../components/HomePage.jsx";
+import { connect } from 'react-redux';
 
 // import { changePage, changeGameID } from "../redux/actions/pages";
 
-  // const templates = props.templates.map(template => {
-  //   return (
-  //     <li>
-  //       {template.name}
-  //       <button onClick={e => props.onDelete(template.id)}>Delete</button>
-  //     </li>
-  //   );
-  // });
+// const mapStateToProps = state => {
+//   return {
+//     //
+//   };
+// };
 
-  // return <ul>{templates}</ul>;
+// const mapDispatchToProps = {
+//   //
+// };
 
-const mapStateToProps = state => {
-  return {
-    //
-  }
+const templates = ['Settlers of Catan', 'Seven Wonders', 'Unearth'];
+
+export default function TemplateList(props) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Games:</th>
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        {templates.map(template => {
+          return (
+            <tr>
+              <td>{template}</td>
+              <td>
+                <button>Start Game</button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
 
-const mapDispatchToProps = {
-  //
-}
-
-function TemplateList(props) {
-  <table>
-    <thead>
-      <tr>
-        <th>Games:</th>
-        <th />
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Settlers of Catan</td>
-        <td>
-          <button>Start Game</button>
-        </td>
-      </tr>
-      <tr>
-        <td>Seven Wonders</td>
-        <td>
-          <button>Start Game</button>
-        </td>
-      </tr>
-      <tr>
-        <td>Unearth</td>
-        <td>
-          <button>Start Game</button>
-        </td>
-      </tr>
-    </tbody>
-  </table>;
-}
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Content);
+// export default connect(mapStateToProps, mapDispatchToProps)(TemplateList);
