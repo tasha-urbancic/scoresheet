b8ac18f8d5d0eec2885ff16277fb60cb066f331c
tasha-urbancic <tasha.urbancic@gmail.com>
2017-10-24 20:35:06 +0000

    added state files for game-page

--- scoresheet-app/src/App.jsx
+++ scoresheet-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import * as ReactBootstrap from "react-bootstrap";
+// import * as ReactBootstrap from "react-bootstrap";
 import Content from "./containers/Content.jsx";
 import NavBar from "./components/NavBar.jsx";
 
@@ -11,11 +11,12 @@ class App extends Component {
       <div>
         <NavBar />
         <Content />
-        <ReactBootstrap.Button bsStyle="success" bsSize="small">
-          Something
-        </ReactBootstrap.Button>
       </div>
     );
   }
 }
 export default App;
+
+{/* <ReactBootstrap.Button bsStyle="success" bsSize="small">
+Something
+</ReactBootstrap.Button> */}
