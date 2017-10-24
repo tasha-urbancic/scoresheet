f9c4020a7866b5b9c1201778b803f076159414f0
Mijung Lee <Mijung@Stephens-MacBook-Air.local>
2017-10-23 22:19:06 -0700

    edited templates action creator to work accordingly with our app

--- scoresheet-app/src/redux/reducers/templates.js
+++ scoresheet-app/src/redux/reducers/templates.js
@@ -1,15 +1,9 @@
 import { ADD_TEMPLATE, DELETE_TEMPLATE } from '../constants/templates';
 
-export default (state = {}, action) => {
+export default (state = [], action) => {
   switch (action.type) {
     case ADD_TEMPLATE:
-      const template = {
-        ...action.template
-      };
-      return {
-        ...state,
-        templates: [...state.templates, template]
-      };
+      return [...state, action.template];
     // case DELETE_TEMPLATE:
     //   return {
     //     ...state,
