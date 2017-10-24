1e8078f2e737a5b32f3b52a00621ff204aef64d6
Mijung Lee <Mijung@Stephens-MacBook-Air.local>
2017-10-23 21:45:07 -0700

    took out delete template case

--- scoresheet-app/src/redux/reducers/templates.js
+++ scoresheet-app/src/redux/reducers/templates.js
@@ -1,24 +1,20 @@
 import { ADD_TEMPLATE, DELETE_TEMPLATE } from '../constants/templates';
 
-let id = 0;
-
 export default (state = {}, action) => {
   switch (action.type) {
     case ADD_TEMPLATE:
       const template = {
-        ...action.template,
-        id: id++
+        ...action.template
       };
-
       return {
         ...state,
         templates: [...state.templates, template]
       };
-    case DELETE_TEMPLATE:
-      return {
-        ...state,
-        templates: state.templates.filter(template => template.id !== action.id)
-      };
+    // case DELETE_TEMPLATE:
+    //   return {
+    //     ...state,
+    //     templates: state.templates.filter(template => template.id !== action.id)
+    //   };
     default:
       return state;
   }
