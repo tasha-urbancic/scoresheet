61f126b1c4cb2e9ee1e263257fc2f832ef9d00f6
Mijung Lee <Mijung@Stephens-MacBook-Air.local>
2017-10-24 13:42:31 -0700

    uncommented res.header for access control

--- scoresheet-http-server/server/server.js
+++ scoresheet-http-server/server/server.js
@@ -11,10 +11,10 @@ const routes = require('./routes');
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  // res.header(
-  //   'Access-Control-Allow-Headers',
-  //   'Origin, X-Requested-With, Content-Type, Accept'
-  // );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   next();
 });
 // app.use(express.static("public"));
