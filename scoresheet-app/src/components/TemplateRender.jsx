831369794384a9b330f68f426bcbd7e6959a423e
lojenny <jennychingyinlo@gmail.com>
2017-10-29 00:34:48 +0000

    delete dead code

--- scoresheet-app/src/components/TemplateRender.jsx
+++ scoresheet-app/src/components/TemplateRender.jsx
@@ -75,9 +75,6 @@ export default class TemplateRender extends Component {
               <div className="form-horizontal">
                 <ScoresheetStructure />
               </div>
-              {/* <div className="col-sm-12">
-                <button className="btn btn-default">Submit Structure</button>
-              </div> */}
             </div>
           </div>
           <div className="row well">
