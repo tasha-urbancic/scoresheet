ac33196872765c8b5234febaadba97c1031dfd47
tasha-urbancic <tasha.urbancic@gmail.com>
2017-10-30 17:55:58 +0000

    added some optional changes to landing page, and also fixed bug with adding new operations

--- scoresheet-app/src/components/LandingPage.jsx
+++ scoresheet-app/src/components/LandingPage.jsx
@@ -10,18 +10,18 @@ export default class LandingPage extends Component {
             src="/scoresheet-app/docs/imgs/logo-first-draft.png"
             className="landing-page-logo"
           />
+          <div className="landing-page-btn">
+            <button className="btn btn-dark btn-lg js-scroll-trigger">
+              <Link to="/templates" className="white-text">
+                Start Playing
+              </Link>
+            </button>
+          </div>
         </div>
-        <div className="landing-page-btn">
-          <button className="btn btn-dark btn-lg js-scroll-trigger">
-            <Link to="/templates" className="white-text">
-              Start Playing
-            </Link>
-          </button>
-        </div>
-        <img
+        {/* <img
           src="/scoresheet-app/docs/imgs/notepad.png"
           className="landing-page-notepad"
-        />
+        /> */}
       </div>
     );
   }
