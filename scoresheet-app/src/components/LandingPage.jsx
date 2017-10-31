2b3ccd74ee0573e701e90863b434746bda28fa4b
tasha-urbancic <tasha.urbancic@gmail.com>
2017-10-31 06:17:13 +0000

    added new font type

--- scoresheet-app/src/components/LandingPage.jsx
+++ scoresheet-app/src/components/LandingPage.jsx
@@ -1,28 +1,25 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class LandingPage extends Component {
-  render() {
-    return (
-      <div id="landing-page">
-        <div>
-          <img
-            src="/scoresheet-app/docs/imgs/logo-first-draft.png"
-            className="landing-page-logo"
-          />
-          <div className="landing-page-btn">
-            <button className="btn btn-dark btn-lg js-scroll-trigger">
-              <Link to="/templates" className="white-text">
-                Start Playing
-              </Link>
-            </button>
-          </div>
-        </div>
-        {/* <img
+	render() {
+		return (
+			<div id="landing-page">
+				<div>
+					<img src="/scoresheet-app/docs/imgs/logo-first-draft.png" className="landing-page-logo" />
+					<div className="landing-page-btn">
+						<button className="btn btn-dark btn-lg js-scroll-trigger">
+							<Link to="/templates" className="white-text">
+								Start Playing
+							</Link>
+						</button>
+					</div>
+				</div>
+				{/* <img
           src="/scoresheet-app/docs/imgs/notepad.png"
           className="landing-page-notepad"
         /> */}
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 }
